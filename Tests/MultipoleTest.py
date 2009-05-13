a307870c3f02d9b645fbe9c82f4de7401ee22d6d
rpmuller <rpmuller@64417113-1622-0410-aef8-ef15d1a3721e>
2009-05-13 20:58:34 +0000

    Multipole works for CH4/6-31G/x; inertial transformation seems to work
    git-svn-id: https://pyquante.svn.sourceforge.net/svnroot/pyquante/trunk@117 64417113-1622-0410-aef8-ef15d1a3721e

--- Tests/MultipoleTest.py
+++ Tests/MultipoleTest.py
@@ -14,21 +14,30 @@ for i in range(nbf):
 
 # Sheesh! I don't have 6-31g in PyQuante
 
-print '6-31G CH4 Z'
+print '6-31G CH4 1'
 ch4 = Molecule('ch4',
                [('C',(0.0, 0.0, 0.0)),
                 ('H',(0.0, 0.0, 1.083658)),
                 ('H',(1.021683, 0.0, -0.361219)),
                 ('H',(-0.510841, 0.884804, -0.361219)),
-                ('H',(-0.510841,-0.884804, -0.361219)),])
-
+                ('H',(-0.510841,-0.884804, -0.361219)),],
+               units='Angs')
 ch4.inertial()
+
+# This should give the same results, but without the inertial coordinate
+# transformation
+#ch4 = Molecule('ch4',
+#               [(6,( 0.0000000000,-0.0000000781, 0.0000001487)),
+#                (1,( 0.0000000000,-0.6085461813,-1.9553066848)),
+#                (1,( 0.0000000000, 2.0463301907, 0.0780254767)),
+#                (1,( 1.6720371128,-0.7188915396, 0.9386397186)),
+#                (1,(-1.6720371128,-0.7188915396, 0.9386397186))])
 
 basis = getbasis(ch4,basis_data='6-31g')
 nbf = len(basis)
 
 for i in range(nbf):
     for j in range(nbf):
-        print i,j,basis[i].multipole(basis[j],0,0,1)
+        print i,j,basis[i].multipole(basis[j],1,0,0)
 
 
