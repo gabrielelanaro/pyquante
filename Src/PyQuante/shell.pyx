483571110b83cab406d3d4d8f2eba5cae0a2da58
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-12-10 17:35:27 +0100

    Added license headers to cython files

--- Src/PyQuante/shell.pyx
+++ Src/PyQuante/shell.pyx
@@ -1,3 +1,14 @@
+#		 This source code is part of the
+#		 PyQuante quantum chemistry suite
+#
+# Written by Gabriele Lanaro, 2009-2010
+# Copyright (c) 2009-2010, Gabriele Lanaro
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
 from contracted_gto cimport ContractedGTO
 cimport shell
 
